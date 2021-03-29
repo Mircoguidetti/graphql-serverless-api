1415e4cfde4583289abce7da47a9da948b411a2c
Mirco Guidetti <mirco@guidetti.co>
2021-03-29 17:18:20 +0100

    minor change

--- src/services/graphql/modules/appointment/resolvers.ts
+++ src/services/graphql/modules/appointment/resolvers.ts
@@ -2,8 +2,8 @@ import { AppointmentProvider } from './provider'
 import { AppointmentInterface } from './interfaces'
 export const resolvers = {
   Mutation: {
-    createAppointment: async (_, args, { dynamoDB, injector }): Promise<AppointmentInterface> => {
-      return injector.get(AppointmentProvider).createAppointment(args, dynamoDB)
+    createAppointment: async (_, args, { dynamoDB }): Promise<AppointmentInterface> => {
+      return AppointmentProvider.createAppointment(args, dynamoDB)
     },
   },
 }
